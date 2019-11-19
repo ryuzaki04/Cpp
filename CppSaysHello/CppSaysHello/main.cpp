6aae3b1b2d82e8160d5e181eb0acd052e3d6f857
Aman Bhardwaj <amanbhardwaj.04091997@gmail.com>
2019-11-19 05:46:34 +0530

    hello cpp

---
+++ CppSaysHello/CppSaysHello/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  CppSaysHello
+//
+//  Created by Aman Bhardwaj on 11/19/19.
+//  Copyright © 2019 Projects. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+class Hello
+{
+public:
+    
+    void hellow()
+    {
+        cout<<"hello worlds! "<<endl;
+    }
+};
+
+
+int main()
+{
+    Hello h;
+    h.hellow();
+}
